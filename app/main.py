220fade419e45c60ecaa309aacd91d55a50dacc8
prakash S <prakashsellathurai@gmail.com>
2025-08-03 15:49:32 +0000

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ import sys
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
+    elif pattern == r"\d":
+        return any(char.isdigit() for char in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
